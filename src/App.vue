e807b7fd9ffdc4b9e6d3dc3d5fb2c705b7056a4c
Andrei <andrei@cubework.com>
2024-09-30 13:34:09 +0800

    Fix merge error

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HeaderVue from './components/Header.vue'
+import { RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
-  <HeaderVue />
-  <!-- <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
+  <HeaderComponent />
   <RouterView />
   
 </template>
